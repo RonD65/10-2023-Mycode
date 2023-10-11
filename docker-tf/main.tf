8c2c41f459b66e6f5923d6a9c9e487dc12bcf0f4
RonD65 <rond65@hotmail.com>
2023-10-11 20:47:40 +0000

    end of day 1

---
+++ docker-tf/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+# Sets the provider #
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+// Sets the resource //
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+/* this end this file.
+Edits will not be accepted */
